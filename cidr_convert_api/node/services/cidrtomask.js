4e5cb7392934894251a051b6a30aa9d03f1af113
FcoMtzHrdez <94252286+FcoMtzHrdez@users.noreply.github.com>
2022-03-17 15:56:03 -0600

    Update cidrtomask.js
    cidr to mask change

--- cidr_convert_api/node/services/cidrtomask.js
+++ cidr_convert_api/node/services/cidrtomask.js
@@ -1,10 +1,10 @@
 export const cidrToMaskFunction = (value) => {
- var mask = [], i, n;
-  for(i=0; i<4; i++) {
-    n = Math.min(value, 8);
+var mask=[];
+  for(var i=0;i<4;i++) {
+    var n = Math.min(value, 8);
     mask.push(256 - Math.pow(2, 8-n));
-    bitCount -= n;
+    value -= n;
   }
-  return mask.join('.')
+  return mask.join('.');
 
 }
