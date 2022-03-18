0905dbff467bb38ede5e1b2b35bd8d6540960a4f
FcoMtzHrdez <94252286+FcoMtzHrdez@users.noreply.github.com>
2022-03-18 03:24:29 -0600

    Update index.js

--- cidr_convert_api/node/routes/index.js
+++ cidr_convert_api/node/routes/index.js
@@ -1,7 +1,7 @@
-import { cidrToMask } from '../services/cidrtomask';
-import { maskToCidr } from '../services/masktocidr';
-import { ipv4Validation } from '../serives/ipv4validation';
-import { health } from '../services/health';
+import { cidrToMask } from './services/cidrtomask';
+import { maskToCidr } from './services/masktocidr';
+import { ipv4Validation } from './serives/ipv4validation';
+import { health } from './services/health';
 
 export const init = (app) => {
     app.get('/cidr-to-mask', cidrToMask);
