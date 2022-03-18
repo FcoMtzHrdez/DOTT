176dcbf48b4973a81324d4a6e909e6391f85123a
FcoMtzHrdez <94252286+FcoMtzHrdez@users.noreply.github.com>
2022-03-18 10:16:24 -0600

    Update cidrtomask.js
    se agrega try catch

--- cidr_convert_api/node/services/cidrtomask.js
+++ cidr_convert_api/node/services/cidrtomask.js
@@ -1,9 +1,13 @@
 export const cidrToMaskFunction = (value) => {
 var mask=[];
   for(var i=0;i<4;i++) {
+    try {
     var n = Math.min(value, 8);
     mask.push(256 - Math.pow(2, 8-n));
-    value -= n;
+    value -= n; 
+    } catch(e){
+      return "Invalid"
+    }
   }
   return mask.join('.');
 
