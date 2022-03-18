dab2bc7d240aca894d8cef3de72a4f64a0e3efd6
FcoMtzHrdez <94252286+FcoMtzHrdez@users.noreply.github.com>
2022-03-18 03:44:19 -0600

    Update masktocidr.js

--- cidr_convert_api/node/services/masktocidr.js
+++ cidr_convert_api/node/services/masktocidr.js
@@ -1,15 +1,10 @@
 export const maskToCidrFunction = (value) => {
- var cidr = ''
-    for (m of value.split('.')) {
-        if (parseInt(m)>255) {throw 'ERROR: Invalid Netmask'} // Check each group is 0-255
-        if (parseInt(m)>0 && parseInt(m)<128) {throw 'ERROR: Invalid Netmask'}
-
-        cidr+=(m >>> 0).toString(2)
-    }
-    // Condition to check for validity of the netmask
-    if (cidr.substring(cidr.search('0'),32).search('1') !== -1) {
-        throw 'ERROR: Invalid Netmask ' + value
-    }
-    return cidr.split('1').length-1
+var maskNodes = value.match(/(\d+)/g);
+var cidr = 0;
+for(var i in maskNodes)
+{
+  cidr += (((maskNodes[i] >>> 0).toString(2)).match(/1/g) || []).length;
+}
+return cidr;
 
 }
